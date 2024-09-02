3b5f9f9d9a5875251641c0c1178f5511ecb44681
Habit-Tracker <sauravbista10@gmail.com>
2024-09-02 08:16:10 +0545

    Added Gitignore and Procfile

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
 from flask import abort
 from flask_gravatar import Gravatar
+import os
 
 def admin_only(f):
     @wraps(f)
@@ -22,21 +23,10 @@ def admin_only(f):
         return f(*args, **kwargs)
     return decorated_function
 
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 login_manager = LoginManager()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+##app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 login_manager.init_app(app)
@@ -51,7 +41,8 @@ def forbidden_error(error):
     return render_template('error.html'), 403
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
+##app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -257,4 +248,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
